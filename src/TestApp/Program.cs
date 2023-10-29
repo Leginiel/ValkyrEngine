c5965e73cd62875d78087269663452fe3e239d2d
Sebastian Berning <hunter.vasitas@googlemail.com>
2023-10-29 11:30:37 +0100

    ValkyrEngine: Adding a console test project
    This will help us to run and create the engine like any other consumer.

---
+++ src/TestApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using ValkyrEngine;
+using Silk.NET.Windowing;
+using Silk.NET.Maths;
+
+namespace TestApp;
+
+internal class Program
+{
+  private static void Main(string[] args)
+  {
+    WindowOptions windowOptions = WindowOptions.DefaultVulkan with
+    {
+      Size = new Vector2D<int>(800, 600),
+      Title = "TestApp"
+    };
+    ValkyrEngineOptions options = new()
+    {
+      WindowOptions = windowOptions,
+      ApplicationName = "TestApp",
+      VertexShaderCode = File.ReadAllBytes("bin/Debug/net8.0/shaders/vert.spv"),
+      FragmentShaderCode = File.ReadAllBytes("bin/Debug/net8.0/shaders/frag.spv")
+    };
+    Game game = new();
+
+    game.Init(options);
+    game.Run();
+  }
+}
