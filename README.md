4c170276c0a0342d75c37dd670abeffa0a33b6f0
Sebastian Berning <hunter.vasitas@googlemail.com>
2023-10-29 11:25:29 +0100

    ValkyrEngine: Create a basic game class. This class will currently just open a blank window, with the settings from the user.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ValkyrEngine
+
+
+## Build requirements
+
+The projects require the [Vulkan SDK](https://www.lunarg.com/vulkan-sdk/) to build/run. The SDK provides the Vulkan validation layers as well as the command line tools to compile the shaders. The projects include a targets file that compiles the shaders.
---
+++ src/ValkyrEngine/Game.cs
@@ -0,0 +1,50 @@
+﻿using Silk.NET.Windowing;
+
+namespace ValkyrEngine;
+
+public class Game : IDisposable
+{
+  private bool _disposedValue;
+  private IWindow? _window;
+
+
+  public void Init(WindowOptions windowOptions)
+  {
+    InitWindow(windowOptions);
+    InitVulkan();
+  }
+  public void Run()
+  {
+    if (_window is null)
+      return;
+
+    _window.Run();
+  }
+
+  public void Dispose()
+  {
+    // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+    Dispose(disposing: true);
+    GC.SuppressFinalize(this);
+  }
+
+  protected virtual void Dispose(bool disposing)
+  {
+    if (!_disposedValue)
+    {
+      if (disposing)
+      {
+        _window?.Dispose();
+      }
+      _disposedValue = true;
+    }
+  }
+  private void InitWindow(WindowOptions windowOptions)
+  {
+    _window = Window.Create(windowOptions);
+    _window.Initialize();
+  }
+  private void InitVulkan()
+  {
+  }
+}
